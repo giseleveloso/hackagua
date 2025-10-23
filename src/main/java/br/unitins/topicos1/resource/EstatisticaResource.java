49d8658a734357b0df88384e6d7fe694441b2597
Gisele de Oliveira <giseleveloso00@gmail.com>
2025-10-22 23:18:58 -0300

    feat: graphic control

--- src/main/java/br/unitins/topicos1/resource/EstatisticaResource.java
+++ src/main/java/br/unitins/topicos1/resource/EstatisticaResource.java
@@ -3,12 +3,14 @@ package br.unitins.topicos1.resource;
 import org.jboss.logging.Logger;
 
 import br.unitins.topicos1.service.EstatisticaService;
+import br.unitins.topicos1.service.GraficoService;
 import br.unitins.topicos1.dto.UsuarioEstatisticaResponseDTO;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -20,6 +22,9 @@ public class EstatisticaResource {
     
     @Inject
     public EstatisticaService estatisticaService;
+    
+    @Inject
+    public GraficoService graficoService;
 
     private static final Logger LOG = Logger.getLogger(EstatisticaResource.class);
 
@@ -36,5 +41,29 @@ public class EstatisticaResource {
         LOG.infof("Calculando estatísticas do usuário: %d", usuarioId);
         UsuarioEstatisticaResponseDTO dto = estatisticaService.calcularEstatisticasUsuario(usuarioId);
         return Response.ok(dto).build();
+    }
+
+    @GET
+    @Path("/grafico/medidor/{medidorId}")
+    public Response graficoMedidor(
+        @PathParam("medidorId") Long medidorId,
+        @QueryParam("metric") String metric,
+        @QueryParam("dataInicio") String dataInicio,
+        @QueryParam("dataFim") String dataFim
+    ) {
+        LOG.infof("Grafico medidor %d metric=%s %s..%s", medidorId, metric, dataInicio, dataFim);
+        return Response.ok(graficoService.graficoMedidor(medidorId, metric, dataInicio, dataFim)).build();
+    }
+
+    @GET
+    @Path("/grafico/usuario/{usuarioId}")
+    public Response graficoUsuario(
+        @PathParam("usuarioId") Long usuarioId,
+        @QueryParam("metric") String metric,
+        @QueryParam("dataInicio") String dataInicio,
+        @QueryParam("dataFim") String dataFim
+    ) {
+        LOG.infof("Grafico usuario %d metric=%s %s..%s", usuarioId, metric, dataInicio, dataFim);
+        return Response.ok(graficoService.graficoUsuario(usuarioId, metric, dataInicio, dataFim)).build();
     }
 }
